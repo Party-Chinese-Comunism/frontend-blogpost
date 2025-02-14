9619609c0aa3e71b2dd93799c34653f971d4a666
Gustavo Lima <limagustavorafael@hotmail.com>
2025-02-13 21:18:24 -0300

    feature: new theme

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { useAuth, AuthProvider } from "./context/auth";
 import { createTheme, ThemeProvider } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SnackbarProvider } from "./context/snackbarContext";
+import theme from "./theme/theme";
 
 // Set up a Router instance
 const router = createRouter({
@@ -24,7 +25,6 @@ declare module "@tanstack/react-router" {
     router: typeof router;
   }
 }
-const theme = createTheme();
 
 function InnerApp() {
   const auth = useAuth();
